b22a52ffbf445716e8e6dffb39896aa4ec172a75
luanmvcosta0 <luanmvcosta@gmail.com>
2024-06-11 11:48:58 -0300

    Pedido de namoro finalizado

---
+++ scripts.js
@@ -0,0 +1,24 @@
+const url = 'https://www.youtube.com/watch?v=Yo2GdogCAZc'
+const btn = document.querySelector('#btn')
+
+function sim() {
+    const win = window.open(url, '_blank')
+    win.focus()
+}
+
+btn.addEventListener('click', () => {
+    openInNewTab(url)
+})
+
+function desvia(t) {
+    var btn = t
+    btn.style.position = 'absolute'
+    btn.style.bottom = geraPosicao(10, 90)
+    btn.style.left = geraPosicao(10, 90)
+    console.log("opa, desviei...")
+}
+
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%"
+}
